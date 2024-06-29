2f4364a1afcc6ee2306970d43c4923c16abc66bd
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-06-28 19:02:04 -0500

    Added coding to show guessed letters, update word in progress and check if players win

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const guessedLettersTracker = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterGuess = document.querySelector("#letter");
-const wordInProgress = document.querySelector(".word-in-progress");
+const wordBeingGuessed = document.querySelector(".word-in-progress");
 const guessesRemaining = document.querySelector(".remaining");
 const guessesRemaingSpan = document.querySelector(".remaining span");
 const guessMessages = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
-const guessedLetters = [];
+const guessedLettersArray = [];
 
+//This function generates the dots for the word to be guessed. One dot per letter.
 const letterDots = function (word) {
     // console.log(word.length);
     // console.log(word[0]);
@@ -19,44 +20,84 @@ const letterDots = function (word) {
         // console.log(numDots[i]);
         // console.log(word[i]);
     };
-    wordInProgress.innerText = numDots.join("");
-    // i = i++;
+    wordBeingGuessed.innerText = numDots.join("");
 };
 
 letterDots(word);
 
+//This listens for the Guess! button to be clicked and then begins the process.
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     guessMessages.innerText = "";
     let guessedLetter = letterGuess.value.toUpperCase();
-    // console.log(guessedLetter);
-    guessInput(guessedLetter);
-    console.log(guessedLetter);
+    const validGuess = validateGuess(guessedLetter);
+    if (validGuess) makeGuess(guessedLetter);
     letterGuess.value = "";
 });
 
-const guessInput = function (guessedLetter) {
+//This function checks for empty input, more than letter, or non-letter inputs.
+const validateGuess = function (input) {
     const acceptedLetter = /[a-zA-Z]/;
-    // console.log(guessedLetter.match(acceptedLetter));
-    if (guessedLetter == "") {
+    if (input == "") {
         guessMessages.innerText = "You didn't enter anything...";
-    } else if (guessedLetter.length > 1) {
+    } else if (input.length > 1) {
         guessMessages.innerText = "Just one letter please";
-    } else if (guessedLetter.match(acceptedLetter) == null) {
+    } else if (input.match(acceptedLetter) == null) {
         guessMessages.innerText = "That is not an acceptable entry";
     } else {
-        if (guessedLetters.includes(guessedLetter)) {
-            guessMessages.innerText = `You already guessed ${guessedLetter}. Try again`;
-        } else {
-            guessMessages.innerText = `You guessed ${guessedLetter}`
-            guessedLetters.push(guessedLetter);
-            console.log(guessedLetters);
-            makeGuess(guessedLetter);
-            return guessedLetter;
-        }
+        return input;
     }
 };
 
+//This function checks if the letter has been guessed already before or not.
 const makeGuess = function (letter) {
-    
- };
+    if (guessedLettersArray.includes(letter)) {
+        guessMessages.innerText = `You already guessed ${letter}. Try again`;
+    } else {
+        guessMessages.innerText = `You guessed ${letter}`;
+        guessedLettersArray.push(letter);
+        displayGuessedLetters(letter);
+        return letter;
+    };
+};
+
+
+//This is the function that displays the letters that have been guessed.
+const displayGuessedLetters = function (letter) {
+    guessedLettersTracker.innerHTML = "";
+    for (let e of guessedLettersArray) {
+        let li = document.createElement("li");
+        li.innerText = e;
+        guessedLettersTracker.append(li);
+    };
+    wordInProgress(letter);
+    console.log(`tracker: ${guessedLettersArray}`);
+};
+
+//This function reveals the letters as they are guessed.
+const wordInProgress = function (letter) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    let wordInDots = (wordBeingGuessed.innerText).split("");
+    console.log(wordInDots);
+    console.log(wordArray);
+    console.log(guessedLettersArray);
+    // for (const e of guessedLettersArray) {
+    wordArray.forEach(function (i, index) {
+        if (i === letter) {
+            wordInDots[index] = letter;
+        };
+    });
+    // };
+    const currentWordInDots = wordInDots.join("");
+    wordBeingGuessed.innerText = currentWordInDots;
+    winOrLoss(currentWordInDots, wordUpper)
+};
+
+
+const winOrLoss = function(currentWordInDots, wordUpper) {
+    if (currentWordInDots === wordUpper) {
+        guessMessages.classList.add("win");
+        guessMessages.outerHTML = '<p class="highlight win">You guessed the word correctly!</p>';
+    }
+};
