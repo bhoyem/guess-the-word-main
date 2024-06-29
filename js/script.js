91340d7560c6839dd38bfbc10c7cdcc04396fe5c
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-06-29 08:49:26 -0500

    Added code to count the guesses and to get a randome word

--- js/script.js
+++ js/script.js
@@ -3,11 +3,34 @@ const guessButton = document.querySelector(".guess");
 const letterGuess = document.querySelector("#letter");
 const wordBeingGuessed = document.querySelector(".word-in-progress");
 const guessesRemaining = document.querySelector(".remaining");
-const guessesRemaingSpan = document.querySelector(".remaining span");
+const guessesRemainingSpan = document.querySelector(".remaining span");
 const guessMessages = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
-const word = "magnolia";
+let word = "funny";
 const guessedLettersArray = [];
+let remainingGuesses = 8;
+
+
+
+//This function pulls a random word from a fetched list of words.
+const getWord = async function () {
+    const request = await fetch(`https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt`);
+    const words = await request.text();
+    // console.log(words);
+    const wordArray = words.split("\n");
+    console.log(wordArray.length);
+
+    const selectRandomWord = function (wordArray) {
+        const randomIndex = Math.floor(Math.random() * wordArray.length);
+        const randomWord = wordArray[randomIndex];
+        console.log(randomIndex);
+        console.log(`| x${randomWord}x |`);
+        word = randomWord;
+    };
+
+    selectRandomWord(wordArray);
+    letterDots(word);
+};
 
 //This function generates the dots for the word to be guessed. One dot per letter.
 const letterDots = function (word) {
@@ -23,7 +46,10 @@ const letterDots = function (word) {
     wordBeingGuessed.innerText = numDots.join("");
 };
 
-letterDots(word);
+
+getWord();
+
+
 
 //This listens for the Guess! button to be clicked and then begins the process.
 guessButton.addEventListener("click", function (e) {
@@ -74,11 +100,12 @@ const displayGuessedLetters = function (letter) {
     console.log(`tracker: ${guessedLettersArray}`);
 };
 
-//This function reveals the letters as they are guessed.
+//This function reveals the letters as they are guessed and determines if the number of guesses will change.
 const wordInProgress = function (letter) {
     const wordUpper = word.toUpperCase();
     const wordArray = wordUpper.split("");
     let wordInDots = (wordBeingGuessed.innerText).split("");
+    let skipCountFlag = 0;
     console.log(wordInDots);
     console.log(wordArray);
     console.log(guessedLettersArray);
@@ -86,18 +113,46 @@ const wordInProgress = function (letter) {
     wordArray.forEach(function (i, index) {
         if (i === letter) {
             wordInDots[index] = letter;
+            skipCountFlag = 1;
         };
     });
     // };
+    if (skipCountFlag == 0) countGuesses();
     const currentWordInDots = wordInDots.join("");
     wordBeingGuessed.innerText = currentWordInDots;
     winOrLoss(currentWordInDots, wordUpper)
 };
 
+//This function decrements the guesses remaining and updates the message accordingly.
+const countGuesses = function () {
+    remainingGuesses--;
+    console.log(`Guesses remaining: ${remainingGuesses}`);
+    if (remainingGuesses <= 0) {
+        guessesRemaining.innerText = `Sorry, you have run out of guesses. Try again?`;
+    } else if (remainingGuesses == 1) {
+        guessesRemainingSpan.innerText = `${remainingGuesses} guess`;
+    } else {
+        guessesRemainingSpan.innerText = `${remainingGuesses} guesses`
+    }
+    return;
+};
 
-const winOrLoss = function(currentWordInDots, wordUpper) {
-    if (currentWordInDots === wordUpper) {
+//This function checks if the player has guessed all the letters.
+const winOrLoss = function (currentWordInDots, answer) {
+    if (currentWordInDots === answer) {
         guessMessages.classList.add("win");
         guessMessages.outerHTML = '<p class="highlight win">You guessed the word correctly!</p>';
-    }
+    } //else {
+    // remainingGuesses--;
+    // console.log(`Guesses remaining: ${remainingGuesses}`);
+    // if (remainingGuesses <= 0) {
+    //     guessesRemaining.innerText = `Sorry, you have run out of guesses. Try again?`;
+    // } else if (remainingGuesses == 1) {
+    //     guessesRemainingSpan.innerText = `${remainingGuesses} guess`;
+    // } else {
+    //     guessesRemainingSpan.innerText = `${remainingGuesses} guesses`
+    // }
+    // }
+    return;
 };
+
